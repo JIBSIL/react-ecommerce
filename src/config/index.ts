c9446b2adf8223a062aa4eaf7ba79331feefbef2
JIBSIL <40243545+JIBSIL@users.noreply.github.com>
2021-02-23 19:42:11 -0500

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,9 @@
 import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
 
 export const wooApi = new WooCommerceRestApi({
-  url: 'https://react-ecommerce.lougiequisel.com',
-  consumerKey: 'ck_64aa28b5e566c317b5a9a809fa6f07b19e646abc',
-  consumerSecret: 'cs_a56e63dc12ab253643dc05a7c6b934024aee55cf',
+  url: 'https://react-ecommerce.crystalvms.com',
+  consumerKey: 'ck_6c8e9a9a8a1ad7539ea32c030e81f36e2d850c84',
+  consumerSecret: 'cs_c69a6656499349cee2644f1e22d2d7adb0f12d94',
   version: 'wc/v3',
   queryStringAuth: true
 });
