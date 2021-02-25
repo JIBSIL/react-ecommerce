4f4ff0eeeb3a930f93bc5fec8c22bbf15b8a3170
JIBSIL <40243545+JIBSIL@users.noreply.github.com>
2021-02-24 19:50:05 -0500

    Update MainCarousel.tsx

--- src/components/MainCarousel/MainCarousel.tsx
+++ src/components/MainCarousel/MainCarousel.tsx
@@ -3,17 +3,17 @@ import './MainCarousel.less';
 
 const MainCarousel = () => {
   return (
-//    <Carousel autoplay>
+    <Carousel autoplay>
       <div>
         <img src="/images/carousel-demo-images/Image01.png" />
       </div>
-      <div>
-        <img src="/images/carousel-demo-images/Image02.png" />
-      </div>
-      <div>
-        <img src="/images/carousel-demo-images/Image03.png" />
-      </div>
-//    </Carousel>
+//      <div>
+//       <img src="/images/carousel-demo-images/Image02.png" />
+//      </div>
+//      <div>
+//        <img src="/images/carousel-demo-images/Image03.png" />
+//      </div>
+    </Carousel>
   );
 };
 
