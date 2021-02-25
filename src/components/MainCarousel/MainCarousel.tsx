64c16e6037834c7713ae4046deabde61db07da77
JIBSIL <40243545+JIBSIL@users.noreply.github.com>
2021-02-24 19:42:42 -0500

    Remove Demo Images and Add Real Carosel Images

--- src/components/MainCarousel/MainCarousel.tsx
+++ src/components/MainCarousel/MainCarousel.tsx
@@ -5,13 +5,13 @@ const MainCarousel = () => {
   return (
     <Carousel autoplay>
       <div>
-        <img src="/images/carousel-demo-images/fashion-1031469_1920.jpg" />
+        <img src="/images/carousel-demo-images/Image01.png" />
       </div>
       <div>
-        <img src="/images/carousel-demo-images/vinyl-records-945396_1920.jpg" />
+        <img src="/images/carousel-demo-images/Image02.png" />
       </div>
       <div>
-        <img src="/images/carousel-demo-images/bazaar-1853361_1920.jpg" />
+        <img src="/images/carousel-demo-images/Image03.png" />
       </div>
     </Carousel>
   );
